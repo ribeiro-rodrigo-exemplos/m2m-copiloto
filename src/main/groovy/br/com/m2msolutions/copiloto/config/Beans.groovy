3dda1a6e883941b06384a431e1b477f81382ce11
Rodrigo Ribeiro <rodrigo.santos@m2msolutions.com.br>
2017-12-14 16:21:00 -0200

    integracao inicial com o copilotoLed

---
+++ src/main/groovy/br/com/m2msolutions/copiloto/config/Beans.groovy
@@ -0,0 +1,16 @@
+package br.com.m2msolutions.copiloto.config
+
+import br.com.m2msolutions.copiloto.led.CopilotoLed
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class Beans{
+
+    @Bean
+    @ConfigurationProperties('redis')
+    CopilotoLed copilotoLed(){
+        new CopilotoLed()
+    }
+}
--- src/main/groovy/br/com/m2msolutions/copiloto/modelo/viagem/ReguladorDeViagem.groovy
+++ src/main/groovy/br/com/m2msolutions/copiloto/modelo/viagem/ReguladorDeViagem.groovy
@@ -1,5 +1,6 @@
 package br.com.m2msolutions.copiloto.modelo.viagem
 
+import br.com.m2msolutions.copiloto.led.CopilotoLed
 import br.com.m2msolutions.copiloto.modelo.Regulacao
 import br.com.m2msolutions.copiloto.modelo.viagem.momento.MomentoViagem
 import br.com.m2msolutions.copiloto.helpers.DateHelper
@@ -12,9 +13,23 @@ class ReguladorDeViagem {
 
     @Autowired
     DateHelper dateUtil
+    @Autowired
+    CopilotoLed copilotoLed
 
     Regulagem regular(MomentoViagem momentoViagem,Regulacao algoritmo){
-       TimeDuration tempoDeRegulagem =  algoritmo.regular momentoViagem
-        new Regulagem(tempoRegulado: tempoDeRegulagem,dateUtil: dateUtil)
+
+       TimeDuration tempoDeRegulagem = algoritmo.regular momentoViagem
+
+        def regulagem = new Regulagem(tempoRegulado: tempoDeRegulagem,dateUtil: dateUtil)
+
+        copilotoLed.enviarComando(
+            momentoViagem?.veiculo?.clienteId,
+            momentoViagem?.veiculo?.veiculoId,
+            momentoViagem?.veiculo?.modulo?.modelo,
+            momentoViagem?.veiculo?.modulo?.identificador,
+            regulagem.tempoReguladoEmMinutosESegundos()
+        )
+
+        regulagem
     }
 }
