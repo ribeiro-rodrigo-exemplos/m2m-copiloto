3c45b99bb66d5d9b7642d6018544b4fb0833f8e5
Rodrigo Ribeiro <ribeiro.rodrigo1989@gmail.com>
2018-02-10 11:30:12 -0200

    resolve problema de nullpointer no cache de viagem

--- src/main/groovy/br/com/m2msolutions/copiloto/servico/ViagemService.groovy
+++ src/main/groovy/br/com/m2msolutions/copiloto/servico/ViagemService.groovy
@@ -25,7 +25,7 @@ class ViagemService {
     @CacheEvict(key="#veiculoId")
     void removerViagemDoVeiculo(Integer veiculoId){}
 
-    @Cacheable(unless = '#result.getEstado()<3')
+    @Cacheable(unless = '#result==null || #result.getEstado()<3')
     Viagem obterViagemDoVeiculo(Integer veiculoId){
 
         def viagem = viagemRepository.findByIdVeiculo veiculoId
